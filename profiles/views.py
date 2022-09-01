d77b3d4c038dafc0b4b1d3e68aa54cb5bc848921
olaism <olaism01@gmail.com>
2022-09-01 05:25:09 -0700

    Added views

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 
+from .models import (
+    Profile,
+    About,
+    Skill,
+    Project
+)
+
 def home(request):
-    pass
+    return render(request, 'profiles/home.html')
 
 def contact(request):
-    pass
+    return render(request, 'profiles/contact.html', {'contact': contact})
 
 def about(request):
-    pass
+    about = About.objects.all()
+    return render(request, 'profiles/about.html', {'about': about})
 
 def skills(request):
-    pass
+    skills = Skill.objects.all()
+    return render(request, 'profiles/skills.html', {'skills': skills})
 
 def projects(request):
-    pass
+    projects = Project.objects.all()
+    return render(request, 'profiles/projects.html', {'projects': projects})
