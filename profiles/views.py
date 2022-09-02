48d6142c3d668c85a0a113bd063f88c9d2498e0e
olaism <olaism01@gmail.com>
2022-09-01 23:35:07 -0700

    Homepage design

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+import os
+
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 from .models import (
     Profile,
@@ -6,11 +9,33 @@ from .models import (
     Skill,
     Project
 )
+from .forms import ContactForm
 
 def home(request):
     return render(request, 'profiles/home.html')
 
 def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            email = form.cleaned_data['email']
+
+            recipients = [os.environ.get('USER_EMAIL')]
+            if subject and message and from_email:
+                try:
+                    send_mail(subject, message, email, recipients, fail_silently=True, 
+                    auth_password=os.environ.get('USER_PASSWORD'))
+                except BadHeaderError:
+                    return HttpResponse('Invalid header found.')
+
+                return redirect('/contact/thanks/')
+            else:
+                return HttpResponse('Make sure all fields are entered and valid.')
+
+    else:
+        form = ContactForm()
     return render(request, 'profiles/contact.html', {'contact': contact})
 
 def about(request):
